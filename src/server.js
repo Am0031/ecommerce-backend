e8e2ec0214bb7ea78f82ee5df618017e32025ba7
Amelie Pira <amelie.pira@gmail.com>
2022-07-02 20:00:26 +0100

    added sequelize connection

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 // import sequelize connection
+const connection = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
+const init = async () => {
+  try {
+    // connect to DB
+    await connection.sync({ force: false });
+    //server listen to PORT
+    app.listen(PORT, () => {
+      console.log(`App listening on port ${PORT}!`);
+    });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to start server | ${error.message}`);
+  }
+};
+
+init();
