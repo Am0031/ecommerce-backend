b40d2be09a0be2d3570c27c0c4d1127bf9e54f08
Amelie Pira <amelie.pira@gmail.com>
2022-07-02 18:10:22 +0100

    added function for route categories>put/:id

--- src/routes/api/category-routes.js
+++ src/routes/api/category-routes.js
@@ -31,7 +31,7 @@ router.get("/:id", async (req, res) => {
       ],
     });
     if (!category) {
-      return res.status(500).json({ message: "Categories not found" });
+      return res.status(404).json({ message: "Categories not found" });
     }
     return res.json(category);
   } catch (error) {
@@ -41,11 +41,12 @@ router.get("/:id", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  // create a new category
+  // create a new category - passing it an object with the key category_name in it >> object=req.body
+  //https://sequelize.org/docs/v6/core-concepts/model-instances/
   try {
     const { category_name } = req.body;
     if (!category_name) {
-      return res.status(500).json({ message: "Unable to create category" });
+      return res.status(400).json({ message: "Unable to create category" });
     }
     const category = await Category.create(req.body);
     return res
@@ -57,8 +58,32 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", async (req, res) => {
+  // update a category - find it first by its `id`. If exists, pass it an object with the new category_name in it and the id
+  try {
+    const { id } = req.params;
+    const category = await Category.findByPk(id);
+
+    if (!category) {
+      return res.status(404).json({ message: "Category not found" });
+    }
+
+    const { category_name } = req.body;
+    if (!category_name) {
+      return res.status(500).json({ message: "Unable to update category" });
+    }
+    const updatedCategory = await Category.update(
+      { category_name },
+      { where: { id } }
+    );
+
+    return res
+      .status(200)
+      .json({ message: "Category updated", updatedCategory: updatedCategory });
+  } catch (error) {
+    console.error(`ERROR | ${error.message}`);
+    return res.status(500).json(error);
+  }
 });
 
 router.delete("/:id", (req, res) => {
